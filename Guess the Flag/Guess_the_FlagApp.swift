57fad68d1e52346fb7d0f12f9b7da8384f7d6a2b
elifert <ertugrulelifnazli@gmail.com>
2025-08-14 16:16:48 +0300

    Initial Commit

---
+++ Guess the Flag/Guess_the_FlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_FlagApp.swift
+//  Guess the Flag
+//
+//  Created by Elif Ertuğrul on 14.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_FlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
